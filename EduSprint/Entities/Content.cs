39228dc921f50f315c2377aa8adeb8733688194d
Khadija <Khadija@MSI>
2024-04-25 15:40:36 +0300

    git commit -m "first commit"

---
+++ EduSprint/Entities/Content.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace EduSprint.Entities
+{
+    public class Content
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        public string HTMLContent { get; set; }
+
+        public string VideoLink { get; set; }
+
+        public CategoryItem CategoryItem { get; set; }
+    }
+}
